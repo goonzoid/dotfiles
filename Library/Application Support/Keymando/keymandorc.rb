9acd1c3ac7915b66061e31ea662ff481e9f5842d
Will Pragnell <wpragnell@gmail.com>
2013-02-18 19:40:06 +0000

    add keymando config file

---
+++ Library/Application Support/Keymando/keymandorc.rb
@@ -0,0 +1,39 @@
+# Start Keymando at login
+# -----------------------------------------------------------
+start_at_login
+
+# Disable Keymando when using these applications
+# -----------------------------------------------------------
+disable "Remote Desktop Connection"
+
+# Basic mapping
+# -----------------------------------------------------------
+only /Chrome/ do
+  map "<Ctrl-t>", "<Cmd-t>"
+  map "<Ctrl-w>", "<Cmd-w>"
+end
+
+# Commands
+# -----------------------------------------------------------
+
+# Command launcher window via Cmd-Space
+map "<Cmd- >" do
+  trigger_item_with(Commands.items, RunRegisteredCommand.new)
+end
+
+# Register commands
+# -----------------------------------------------------------
+command "Volume Up" do
+  `osascript -e 'set volume output volume (output volume of (get volume settings) + 7)'`
+end
+
+command "Volume Down" do
+  `osascript -e 'set volume output volume (output volume of (get volume settings) - 7)'`
+end
+
+# Repeat last command via Cmd-.
+map "<Cmd-.>", RunLastCommand.instance
+
+# -----------------------------------------------------------
+# Visit http://keymando.com to see what else Keymando can do!
+# -----------------------------------------------------------
