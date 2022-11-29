537ab69552c5b6dc7e612575b5886f5b3d4595a8
Will Pragnell <wpragnell@gmail.com>
2022-11-28 16:10:24 -0800

    linux: mkdir ~/.local/bin when installing

--- install_linux.sh
+++ install_linux.sh
@@ -6,6 +6,7 @@ sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt update
 sudo apt install -y git stow zsh fd-find tmux vim python3-pip
 chsh -s "$(command -v zsh)"
+mkdir -p ~/.local/bin
 ln -f -s "$(command -v fdfind)" ~/.local/bin/fd
 pip3 install --user git+https://github.com/cpbotha/xdg-open-wsl
 
