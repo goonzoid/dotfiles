b2bd8bfa2a6625ee0fae34189b0ff266be6428aa
Will Pragnell <wpragnell@gmail.com>
2023-07-22 18:49:19 -0700

    vim: all lua baby!

--- nvim/init.lua
+++ nvim/init.lua
@@ -80,14 +80,75 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
 })
 
 vim.api.nvim_create_autocmd('BufWritePost', {
-  pattern = { 'init.vim', 'lua.lua' },
-  command = [[source $MYVIMRC]],
+  pattern = 'init.lua',
+  command = [[source $MYVIMRC | edit ~/dotfiles/nvim/init.lua]],
 })
 
 
 -------------------------------
 -- plugin config
 -------------------------------
+
+vim.cmd([[
+call plug#begin()
+
+" basics
+Plug 'tpope/vim-sensible'
+Plug 'rebelot/kanagawa.nvim'
+
+" treesitter
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'nvim-treesitter/playground'
+
+" search and file navigation
+Plug 'tpope/vim-vinegar'
+Plug 'wincent/ferret'
+Plug '~/.fzf' | Plug 'junegunn/fzf.vim'
+
+" editing and navigation helpers
+Plug 'axelf4/vim-strip-trailing-whitespace'
+Plug 'godlygeek/tabular'
+Plug 'junegunn/goyo.vim'
+Plug 'machakann/vim-swap'
+Plug 'milkypostman/vim-togglelist'
+Plug 'romainl/vim-cool' " automatic :noh
+Plug 'tpope/vim-abolish' " cr{c,m,s} to coerce to {camel,mixed,snake} case (and more)
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-endwise'
+Plug 'tpope/vim-repeat'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-unimpaired'
+
+" git
+Plug 'mhinz/vim-signify'
+Plug 'ruanyl/vim-gh-line'
+Plug 'tpope/vim-fugitive'
+
+" ctags
+if executable('ctags')
+  Plug 'ludovicchabant/vim-gutentags'
+endif
+Plug 'majutsushi/tagbar'
+
+" lsp and linting
+Plug 'neovim/nvim-lspconfig'
+Plug 'dense-analysis/ale'
+Plug 'williamboman/mason.nvim', {'do': ':MasonUpdate'}
+Plug 'williamboman/mason-lspconfig.nvim'
+
+" system integration
+Plug 'jpalardy/vim-slime'
+Plug 'preservim/vimux'
+Plug 'tpope/vim-eunuch'
+Plug 'wellle/tmux-complete.vim'
+
+" supercollider and tidal
+Plug 'davidgranstrom/scnvim'
+Plug 'davidgranstrom/scnvim-tmux'
+Plug 'tidalcycles/vim-tidal'
+
+call plug#end()
+]])
 
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
