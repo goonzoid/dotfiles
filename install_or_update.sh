64d2a12030a4e79431cd8be0843a25062b186bfa
Will Pragnell <wpragnell@gmail.com>
2023-09-08 15:54:49 -0700

    zsh: use my fork of zsh-autopair for now

--- install_or_update.sh
+++ install_or_update.sh
@@ -19,7 +19,8 @@ mkdir -p ~/.local/share/zplugins
 clone_if_not_present git@github.com:zsh-users/zsh-syntax-highlighting ~/.local/share/zplugins/zsh-syntax-highlighting
 clone_if_not_present git@github.com:zsh-users/zsh-autosuggestions ~/.local/share/zplugins/zsh-autosuggestions
 clone_if_not_present git@github.com:sindresorhus/pure ~/.local/share/zplugins/pure
-clone_if_not_present git@github.com:hlissner/zsh-autopair ~/.local/share/zplugins/zsh-autopair
+# use my fork until https://github.com/hlissner/zsh-autopair/pull/34 is merged (or #18 is fixed)
+clone_if_not_present git@github.com:goonzoid/zsh-autopair ~/.local/share/zplugins/zsh-autopair
 
 stow -t ~ zsh
 
